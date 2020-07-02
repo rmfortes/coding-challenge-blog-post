eb0d85443493caf88ab4c42d3d18d1de7c617415
REBECCA_FORTES <RMFORTES@UMICH.EDU>
2020-07-02 19:41:20 -0400

    linked github repo to local repo

---
+++ script.js
@@ -0,0 +1,10 @@
+let num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+let num2 = [5, 6, 7, 8, 3, 0, 9];
+let num3 = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4];
+let num4 = [1, 8, 0, 0, 4, 4, 4, "for", "get", "it"];
+
+function formatPhoneNumber() {
+  console.log("Hello");
+}
+
+formatPhoneNumber();
