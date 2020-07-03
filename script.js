f243f3a74b3083e3ff8b2403fee58d4b8243c97c
REBECCA_FORTES <RMFORTES@UMICH.EDU>
2020-07-03 11:16:17 -0400

    fixed conditions

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-// let digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+let digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 // let digits = [5, 6, 7, 8, 3, 0, 9];
 // let digits = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4];
-
-let digits = [1, 8, 0, 0, "N", "O", "P", "E"];
+// let digits = [1, 8, 0, 0, "N", "O", "P", "E"];
 function formatPhoneNumber() { 
-    if (digits.length === 10 && digits.some(isNaN)) {
+    if (digits.length === 10 && !digits.some(isNaN)) {
         let phoneNumber = digits.join("");
     
         let areaCode = phoneNumber.slice(0, 3);
